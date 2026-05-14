a14a11041289a4f77c571ff7c8ae715b9cd534d0
Your Name <you@example.com>
2026-05-14 16:44:50 +0530

    updated frontend

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ if (Data.testCredentials) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <App />
 )
