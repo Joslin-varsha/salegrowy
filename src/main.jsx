72055a4d540f14ac8f4ff506354cac74f16749d6
Your Name <you@example.com>
2026-06-15 13:15:40 +0530

    Updated UI design

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,27 @@ import './index.css'
 import App from './App.jsx'
 import Data from './data/data.js'
 
+// Suppress browser console warnings for closing/closed WebSockets (e.g. from Pusher cleanup)
+if (typeof window !== 'undefined' && window.WebSocket) {
+  const originalSend = window.WebSocket.prototype.send;
+  window.WebSocket.prototype.send = function (data) {
+    if (this.readyState === 1) { // OPEN
+      try {
+        originalSend.call(this, data);
+      } catch (e) {}
+    }
+  };
+
+  const originalClose = window.WebSocket.prototype.close;
+  window.WebSocket.prototype.close = function (code, reason) {
+    if (this.readyState === 0 || this.readyState === 1) { // CONNECTING or OPEN
+      try {
+        originalClose.call(this, code, reason);
+      } catch (e) {}
+    }
+  };
+}
+
 // Inject test credentials from Data object
 if (Data.testCredentials) {
   Object.entries(Data.testCredentials).forEach(([key, value]) => {
